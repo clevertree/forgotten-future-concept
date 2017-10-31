00f4b3cbde0cbdbacbe8c313179390d3314e1aa5
Ari Asulin <ari.asulin@gmail.com>
2017-10-31 16:48:45 -0700

    fixes

--- site/assets/site.js
+++ site/assets/site.js
@@ -7,6 +7,9 @@ document.addEventListener("DOMContentLoaded", function() {
     buttons = document.getElementsByClassName('page-button-subscribe');
     for(i=0; i<buttons.length; i++) buttons[i].addEventListener('click', siteButtonSubscribe);
 
+    buttons = document.getElementsByClassName('toggle-page-menu');
+    for(i=0; i<buttons.length; i++) buttons[i].addEventListener('click', togglePageMenu);
+
     // console.log("Buttons configured", buttons);
 
     function siteButtonPlay(e) {
@@ -16,6 +19,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function siteButtonSubscribe(e) {
         document.location.href = 'site/subscribe.html';
+    }
+
+    function togglePageMenu(e) {
+        document.body.classList.toggle('menu');
     }
 
 });
