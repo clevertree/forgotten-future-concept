a1e14fa4721a81d88476fbe2b4be8605db0918ab
Ari Asulin <ari.asulin@gmail.com>
2017-10-30 21:26:06 -0700

    fixes

---
+++ site/assets/site.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var buttons, i;
+
+    buttons = document.getElementsByClassName('page-button-play');
+    for(i=0; i<buttons.length; i++) buttons[i].addEventListener('click', siteButtonPlay);
+
+    buttons = document.getElementsByClassName('page-button-subscribe');
+    for(i=0; i<buttons.length; i++) buttons[i].addEventListener('click', siteButtonSubscribe);
+
+    console.log("Buttons configured", buttons);
+
+    function siteButtonPlay(e) {
+        relay('INCLUDE game/loader.js');
+        relay('PLAY');
+    }
+
+    function siteButtonSubscribe(e) {
+        document.location.href = 'site/subscribe.html';
+    }
+
+});
